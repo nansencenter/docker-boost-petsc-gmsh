eb4ec227b4d64bfbf0683c5c5e7dae922d22887f
akorosov <korosov@gmail.com>
2018-08-14 10:38:41 +0200

    update_bash

--- update_bash.sh
+++ update_bash.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-echo "export GMSH_DIR=/opt/local/gmsh/3.06" >> /root/.bashrc
+CFG_FILE=/root/nextsim_libs.src
+echo "export GMSH_DIR=/opt/local/gmsh/3.06" >> $CFG_FILE
 
-echo "export NETCDF_DIR=/usr/lib/x86_64-linux-gnu/" >> /root/.bashrc
+echo "export NETCDF_DIR=/usr/lib/x86_64-linux-gnu/" >> $CFG_FILE
 
-echo "export PETSC_DIR=/opt/local/petsc" >> /root/.bashrc
-echo "export PETSC_ARCH=arch-linux2-c-opt" >> /root/.bashrc
+echo "export PETSC_DIR=/opt/local/petsc" >> $CFG_FILE
+echo "export PETSC_ARCH=arch-linux2-c-opt" >> $CFG_FILE
 
-echo "export BOOST_INCDIR=/usr/include/boost" >> /root/.bashrc
-echo "export BOOST_LIBDIR=/usr/lib/x86_64-linux-gnu" >> /root/.bashrc
+echo "export BOOST_INCDIR=/usr/include/boost" >> $CFG_FILE
+echo "export BOOST_LIBDIR=/usr/lib/x86_64-linux-gnu" >> $CFG_FILE
 
-echo "export OPENMPI_LIB_DIR=/usr/lib/x86_64-linux-gnu" >> /root/.bashrc
-echo "export OPENMPI_INCLUDE_DIR=/usr/lib/openmpi/include" >> /root/.bashrc
+echo "export OPENMPI_LIB_DIR=/usr/lib/x86_64-linux-gnu/openmpi/lib" >> $CFG_FILE
+echo "export OPENMPI_INCLUDE_DIR=/usr/lib/x86_64-linux-gnu/openmpi/include" >> $CFG_FILE
 
-echo 'export LD_LIBRARY_PATH=$BOOST_LIBDIR:$GMSH_DIR/lib' >> /root/.bashrc
+echo 'export LD_LIBRARY_PATH=$BOOST_LIBDIR:$GMSH_DIR/lib' >> $CFG_FILE
